0dd62d9a5d18b792c87bcac575e3364ca7aa977f
TiyasAyuLestari <tiyasayulestari71@gmail.com>
2025-11-04 13:00:30 +0700

    Add feature delete student

--- resources/views/admin/student/index.blade.php
+++ resources/views/admin/student/index.blade.php
@@ -39,10 +39,16 @@
                   <a href="{{ route('admin.students.edit', $student->id) }}" class="btn btn-warning btn-sm">
                     <i class="fas fa-edit"></i> Edit
                   </a>
-                  <form action="{{ route('admin.students.destroy', $student->id) }}" method="POST" class="d-inline">
+                  <form
+                    action="{{ route('admin.students.destroy', $student->id) }}"
+                    method="POST"
+                    class="d-inline"
+                    onsubmit="return confirm('Apakah kamu yakin ingin menghapus data siswa ini?')"
+                  >
                     @csrf
                     @method('DELETE')
-                    <button type="submit" class="btn btn-danger btn-sm" onclick="return confirm('Apakah Anda yakin ingin menghapus siswa ini?')">
+
+                    <button class="btn btn-danger btn-sm">
                       <i class="fas fa-trash"></i> Hapus
                     </button>
                   </form>
